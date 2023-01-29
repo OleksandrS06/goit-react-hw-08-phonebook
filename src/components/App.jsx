6ec75d929d72ddb426f9cc81a58f54665e0f2690
OleksandrS06 <112249299+OleksandrS06@users.noreply.github.com>
2023-01-29 19:48:56 +0100

    delt more garbg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,6 @@ import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import HomePage from 'pages/HomePage/HomePage';
-import RegisterPage from 'pages/RegisterPage/RegisterPage';
-import LoginPage from 'pages/LoginPage/LoginPage';
 import { useDispatch } from 'react-redux';
 import { refreshUser } from 'redux/auth/operations';
 import PageNotFound from 'pages/404/404';
